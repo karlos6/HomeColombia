50391da3a12f4b20ba2bcb8f31b127feb6561e2d
karlos6 <Carlos.Keller.M@gmail.com>
2020-02-05 21:43:33 -0500

    creacion de validacion typescript

--- client/src/app/modules/security/security.module.ts
+++ client/src/app/modules/security/security.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { SecurityRoutingModule } from './security-routing.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
 import { RegisterComponent } from './register/register.component';
 
 
@@ -9,7 +9,9 @@ import { RegisterComponent } from './register/register.component';
   declarations: [RegisterComponent],
   imports: [
     CommonModule,
-    SecurityRoutingModule
+    SecurityRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class SecurityModule { }
