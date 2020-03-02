92564a8893c87721f6fd719934b2c06b40a636de
OMicolta <oivar.1701818060@ucaldas.edu.co>
2020-03-01 21:10:18 -0500

    Implementacion de guards en register, department, city e inmueble

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -23,17 +23,17 @@ const routes: Routes = [
   },
   {
     path: 'department',
-    loadChildren: './modules/parameters/department/department.module#DepartmentModule',
+    loadChildren: './modules/parameters/department/department.module#DepartmentModule' ,
     canActivate: [AuthenticationRequiredGuard]    
   },
   {
     path: 'city',
-    loadChildren: './modules/parameters/city/city.module#CityModule',
+    loadChildren: './modules/parameters/city/city.module#CityModule' ,
     canActivate: [AuthenticationRequiredGuard]    
   },
   {
     path: 'inmueble',
-    loadChildren: './modules/administrator/inmueble/inmueble.module#InmuebleModule',
+    loadChildren: './modules/administrator/inmueble/inmueble.module#InmuebleModule' ,
     canActivate: [AuthenticationRequiredGuard]
   },
   {
