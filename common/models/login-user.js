5f97e43aa231f311e02170f16513a725cd75cb1c
karlos6 <Carlos.Keller.M@gmail.com>
2020-03-06 17:37:42 -0500

    creacion dataSource Email y funcion para envio de mensajes por gmail desde la api de loopback

--- common/models/login-user.js
+++ common/models/login-user.js
@@ -1,5 +1,47 @@
 'use strict';
 
-module.exports = function(Loginuser) {
+module.exports = function (Loginuser) {
+    Loginuser.sendEmail = (message,  subject, emailAddresses, cb) => {
+        Loginuser.app.models.Email.send({
+            to: emailAddresses,
+            from: 'karlos.keller.97@gmail.com',
+            subject: subject,
+            text: message,
+            html: message
+        }, function (err, mail) {
+            console.log('email sent!');
+            if (err) return err;
+        });
+        cb(null, 'message sent' + message)
+    }
 
+    Loginuser.remoteMethod(
+        'sendEmail',
+        {
+            http: {
+                path: '/sendEmail', verb: 'get'
+            },
+
+            description: ["Api to send Email message by MyModel."]
+            ,
+            accepts: [
+                {
+                    arg: 'message',
+                    type: 'string',
+                    required: true
+                },
+                {
+                    arg: 'subject',
+                    type: 'string',
+                    required: true
+                },
+                {
+                    arg: 'emailAddresses',
+                    type: 'string',
+                    required: true
+                }
+            ],
+            returns: { arg: 'reponse', type: 'string'}
+        }
+    );
 };
